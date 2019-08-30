fe7d3aa17408393f130ed4ee38a22dac69ab4ff6
manayshan <manay@shankar0506@gmail.com>
2019-08-30 14:16:39 +0530

    Final Submit

---
+++ src/ShowSubscriber.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import './ShowSubscriber.css';
+import {Link} from 'react-router-dom';
+
+class ShowSubscriber extends Component {
+
+  onDeletedClick = (subscriberId) => {
+    this.props.deleteSubscriberHandler(subscriberId); 
+  }
+  render() {
+          return (
+        <div>
+            <Header heading="Phone Directory"/>
+            <div className='component-body-container'></div>
+            <Link to='/add'><button className='custom-btn add-btn'>Add</button></Link>
+            <div className='grid-container heading-container'>
+              <span className='grid-item name-heading'>Name</span><br/>
+              <span className='grid-item phone-heading'>Phone</span>
+            </div>
+
+            {
+                this.props.subscribersList.map(sub => {
+                  return <div key={sub.id} className="grid-container">
+                    <span className="grid-item">{sub.name}</span>
+                    <span className="grid-item">{sub.phone}</span>
+                    <span className="grid-item action-btn-container">
+                      <button className="custom-btn delete-btn" onClick={this.onDeletedClick.bind(this, sub.id)}>Delete</button>
+                    </span>
+                  </div>
+                })
+              }
+        </div>
+      );
+  }
+}
+
+export default ShowSubscriber;
